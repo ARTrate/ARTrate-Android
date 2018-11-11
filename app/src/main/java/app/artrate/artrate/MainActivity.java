950d1989d7a17c3b61e4658e0a80b8f18a58d8ec
Maximilian Rohland <max@rohlandm.de>
2018-11-11 16:59:25 +0100

    worked on mainactivity, next step: show bt devices

--- app/src/main/java/app/artrate/artrate/MainActivity.java
+++ app/src/main/java/app/artrate/artrate/MainActivity.java
@@ -1,9 +1,12 @@
 package app.artrate.artrate;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter BA = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
