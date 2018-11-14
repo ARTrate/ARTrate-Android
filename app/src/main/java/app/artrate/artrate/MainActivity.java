ecf55f1d4e2c60b5060ea8141550ed1e1f52cdb2
Maximilian Rohland <max@rohlandm.de>
2018-11-14 14:50:45 +0100

    listview

--- app/src/main/java/app/artrate/artrate/MainActivity.java
+++ app/src/main/java/app/artrate/artrate/MainActivity.java
@@ -5,22 +5,27 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView btList;
     private static final int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter BA;
+
+    ListView btList;
+    ArrayAdapter<BluetoothDevice> btDevices;
+    ArrayAdapter<String> btNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btList = (TextView) findViewById(R.id.btList);
         BA = BluetoothAdapter.getDefaultAdapter();
         CheckBluetoothState();
     }
@@ -30,16 +35,27 @@ public class MainActivity extends AppCompatActivity {
         // If it isn't turned on, request to turn it on
         // List paired devices
         if(BA==null) {
-            btList.append("\nBluetooth NOT supported. Aborting.");
             return;
         } else {
             if (BA.isEnabled()) {
 
                 // Listing paired devices
                 Set<BluetoothDevice> devices = BA.getBondedDevices();
+                ArrayList<BluetoothDevice> deviceList = new ArrayList<BluetoothDevice>();
+                ArrayList<String> deviceNames = new ArrayList<String>();
+                deviceList.addAll(devices);
+
+                btDevices = new ArrayAdapter<BluetoothDevice>(this, android.R.layout.simple_list_item_single_choice, deviceList);
+
                 for (BluetoothDevice device : devices) {
-                    btList.append("\n  Device: " + device.getName() + ", " + device);
+                   // btText.append("\n  Device: " + device.getName() + ", " + device);
+                    deviceNames.add(device.getName());
                 }
+                btNames = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, deviceNames);
+
+                btList = (ListView) findViewById(R.id.btList);
+                btList.setAdapter(btDevices);
+
             } else {
                 //Prompt user to turn on Bluetooth
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
