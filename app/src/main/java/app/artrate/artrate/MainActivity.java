07b77aadb917ee3c19980b1714d21deb4b0aa399
PeterHae <peter_h@hotmail.de>
2018-11-23 01:38:51 +0100

    can scan for devices but not yet read values...
    warning messy code

--- app/src/main/java/app/artrate/artrate/MainActivity.java
+++ app/src/main/java/app/artrate/artrate/MainActivity.java
@@ -5,22 +5,50 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import com.polidea.rxandroidble2.RxBleClient;
+import com.polidea.rxandroidble2.RxBleDevice;
+import com.polidea.rxandroidble2.scan.ScanFilter;
+import com.polidea.rxandroidble2.scan.ScanSettings;
 
 import java.util.ArrayList;
-import java.util.Set;
+import java.util.UUID;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter BA;
+    private RxBleClient rxBleClient;
+    private Disposable scanSubscription;
+    private Disposable gattSubscription;
+    public byte[] hr;
+
+    public class HRResult {
+        final byte[] hrService;
+        final byte[] hrMeas;
+        final byte[] hrControl;
+
+        HRResult(byte[] hrService, byte[] hrMeas, byte[] hrControl){
+            this.hrControl = hrControl;
+            this.hrMeas = hrMeas;
+            this.hrService = hrService;
+        }
+    }
 
     ListView btList;
-    ArrayAdapter<BluetoothDevice> btDevices;
-    ArrayAdapter<String> btNames;
+    ArrayAdapter<RxBleDevice> deviceList;
+    //ArrayAdapter<String> btNames;
+    //ArrayList<String> deviceNames = new ArrayList<>();
+    ArrayList<RxBleDevice> bleDevices = new ArrayList<>();
+    int selectedDevice = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +56,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BA = BluetoothAdapter.getDefaultAdapter();
-        CheckBluetoothState();
+        initBT();
+        //btNames = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, deviceNames);
+        deviceList = new ArrayAdapter<RxBleDevice>(this, android.R.layout.simple_list_item_single_choice);
+        btList = (ListView) findViewById(R.id.btList);
+        btList.setAdapter(deviceList);
+        btList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedDevice = position;
+            Log.d("Selected Devise is", " " + selectedDevice);
+            getHRData();
+        });
+        rxBleClient = rxBleClient.create(this);
+
+        scanSubscription = rxBleClient.scanBleDevices(
+                new ScanSettings.Builder()
+                    .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                    .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES).build()
+        ).subscribe(
+                rxBleScanResult -> {
+                    RxBleDevice tmp = rxBleScanResult.getBleDevice();
+                    Log.v("Dev found", tmp.getName());
+                    if(tmp.getName() != null && !bleDevices.contains(tmp)){
+                        bleDevices.add(tmp);
+                        //deviceNames.add(tmp.getName());
+                        deviceList.add(tmp);
+                    }
+                },
+                throwable -> {
+                    Log.e("Error: ", throwable.getMessage());
+                }
+        );
     }
 
-    private void CheckBluetoothState() {
+    private void initBT() {
         // Checks for the Bluetooth support and then makes sure it is turned on
         // If it isn't turned on, request to turn it on
         // List paired devices
@@ -39,23 +96,7 @@ public class MainActivity extends AppCompatActivity {
             return;
         } else {
             if (BA.isEnabled()) {
-
-                // Listing paired devices
-                Set<BluetoothDevice> devices = BA.getBondedDevices();
-                ArrayList<BluetoothDevice> deviceList = new ArrayList<BluetoothDevice>();
-                ArrayList<String> deviceNames = new ArrayList<String>();
-                deviceList.addAll(devices);
-
-                btDevices = new ArrayAdapter<BluetoothDevice>(this, android.R.layout.simple_list_item_single_choice, deviceList);
-
-                for (BluetoothDevice device : devices) {
-                   // btText.append("\n  Device: " + device.getName() + ", " + device);
-                    deviceNames.add(device.getName());
-                }
-                btNames = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, deviceNames);
-
-                btList = (ListView) findViewById(R.id.btList);
-                btList.setAdapter(btDevices);
+                return;
 
             } else {
                 //Prompt user to turn on Bluetooth
@@ -65,13 +106,54 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private UUID convertFromInteger(int i) {
+
+        final long MSB = 0x0000000000001000L;
+        final long LSB = 0x800000805f9b34fbL;
+        long value = i & 0xFFFFFFFF;
+        return new UUID(MSB | (value << 32), LSB);
+
+    }
+
+    public void getHRData(){
+        if(selectedDevice >= 0) {
+            /*gattSubscription = bleDevices.get(selectedDevice).establishConnection(false)
+                    .flatMapSingle(rxBleConnection -> rxBleConnection.readCharacteristic(convertFromInteger(0x2A37)))
+                    .subscribe(
+                            characteristicValue -> {
+                                Log.v("got val", characteristicValue.toString());
+                                hr = characteristicValue;
+                            },
+                            throwable -> {
+                                Log.e("Gatt Error", throwable.getMessage());
+                            }
+                    );*/
+            gattSubscription = bleDevices.get(selectedDevice).establishConnection(false)
+                    .flatMapSingle(rxBleConnection -> rxBleConnection.readCharacteristic(convertFromInteger(0x180D)))
+                    .subscribe(
+                            services -> {
+                                Log.v("Offered Services", services.toString());
+
+                            },
+                            throwable -> {
+                                Log.e("Gatt Error", throwable.getMessage());
+                            }
+                    );
+        }
+    }
+
     /* It is called when an activity completes.*/
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_ENABLE_BT) {
-            CheckBluetoothState();
+            initBT();
         }
+    }
+
+    public void OnConnectClick(View view) {
+        scanSubscription.dispose();
+        startOsc(view);
     }
 
     public void startOsc(View view) {
